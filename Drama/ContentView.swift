fba6c6b25d69bfff9321cc0bae0f0743522b7d72
ice <XunMengWinter@gmail.com>
2026-03-09 19:47:31 +0800

    Initial Commit

---
+++ Drama/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Drama
+//
+//  Created by ice on 9/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Drama/DramaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DramaApp.swift
+//  Drama
+//
+//  Created by ice on 9/3/26.
+//
+
+import SwiftUI
+
+@main
+struct DramaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
